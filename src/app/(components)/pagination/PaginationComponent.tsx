c566652a0cda0a3df66087a170d861e12d6ab62b
david-kasprak <kasprakdavid@gmail.com>
2024-11-16 16:57:14 +0200

    in progress, version 0.1, mac

---
+++ src/app/(components)/pagination/PaginationComponent.tsx
@@ -0,0 +1,41 @@
+// components/Pagination.tsx
+'use client';
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+type PaginationProps = {
+    currentPage: number;
+    totalPages: number;
+};
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages }) => {
+    const router = useRouter();
+
+    const goToPage = (page: number) => {
+        if (page < 1 || page > totalPages) return;
+        router.push(`/movies?page=${page}`);
+    };
+
+    return (
+        <div className="pagination">
+            <button
+                disabled={currentPage === 1}
+                onClick={() => goToPage(currentPage - 1)}
+            >
+                Prev
+            </button>
+            <span>
+                Page {currentPage} of {totalPages}
+            </span>
+            <button
+                disabled={currentPage === totalPages}
+                onClick={() => goToPage(currentPage + 1)}
+            >
+                Next
+            </button>
+        </div>
+    );
+};
+
+export default Pagination;
--- src/app/(pages)/movies/page.tsx
+++ src/app/(pages)/movies/page.tsx
@@ -1,31 +1,99 @@
+// // app/movies/page.tsx
+// import React from 'react';
+// import './movies-styles.css';
+//
+// type Movie = {
+//     id: number;
+//     title: string;
+//     overview: string;
+//     poster_path: string;
+// };
+//
+// const MoviesPage = async () => {
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             accept: 'application/json',
+//             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOWY5MTUyMDE4YzdlYzNlYzFiMzBkODM5ZGEzYWMzOCIsIm5iZiI6MTczMTY2NTM4OC40NTM1ODQ3LCJzdWIiOiI2NzM2NjhkN2ExMDJkMjY5YzAzZjA3NTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.n0W00bdvMbhWdJ9OtoWXLcFM8nViAB4RHVy3zNbgtd8',
+//         },
+//     };
+//
+//     // Делаем запрос к API
+//     const response = await fetch(
+//         'https://api.themoviedb.org/3/discover/movie',
+//         options
+//     );
+//     const data = await response.json();
+//
+//     // Передаем полученные данные в компонент
+//     const movies: Movie[] = data.results || [];
+//
+//     return (
+//         <div>
+//             <h1>Movies Page</h1>
+//             {movies.length === 0 ? (
+//                 <p>No movies found</p>
+//             ) : (
+//                 <div className={'movies-container'}>
+//                     {movies.map((movie) => (
+//                         <div className={'movie-box'} key={movie.id}>
+//                             <img className={'movie-poster'} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+//                             <h2>{movie.title}</h2>
+//                             <p>{movie.overview}</p>
+//                         </div>
+//                     ))}
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
+//
+// export default MoviesPage;
+
 // app/movies/page.tsx
 import React from 'react';
 import './movies-styles.css';
+import Pagination from "@/app/(components)/pagination/PaginationComponent";
 
 type Movie = {
     id: number;
     title: string;
     overview: string;
+    poster_path: string;
 };
 
-const MoviesPage = async () => {
+type MovieData = {
+    results: Movie[];
+    total_pages: number;
+    page: number;
+};
+
+type MoviesPageProps = {
+    movies: Movie[];
+    totalPages: number;
+    currentPage: number;
+};
+
+const MoviesPage = async ({ searchParams }: { searchParams: { page?: string } }) => {
+    const page = searchParams.page ? parseInt(searchParams.page) : 1;
+
     const options = {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOWY5MTUyMDE4YzdlYzNlYzFiMzBkODM5ZGEzYWMzOCIsIm5iZiI6MTczMTY2NTM4OC40NTM1ODQ3LCJzdWIiOiI2NzM2NjhkN2ExMDJkMjY5YzAzZjA3NTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.n0W00bdvMbhWdJ9OtoWXLcFM8nViAB4RHVy3zNbgtd8',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOWY5MTUyMDE4YzdlYzNlYzFiMzBkODM5ZGEzYWMzOCIsIm5iZiI6MTczMTc1MjMyMC43NDI4MzA4LCJzdWIiOiI2NzM2NjhkN2ExMDJkMjY5YzAzZjA3NTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.VVNRd2McVF9BKtesmqnX7tnUGpyh9ZnqWUdHq24TGFk', // Замените на ваш ключ
         },
     };
 
-    // Делаем запрос к API
+    // Делаем запрос к API с пагинацией
     const response = await fetch(
-        'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+        `https://api.themoviedb.org/3/discover/movie?page=${page}`,
         options
     );
-    const data = await response.json();
+    const data: MovieData = await response.json();
 
-    // Передаем полученные данные в компонент
-    const movies: Movie[] = data.results || [];
+    const movies = data.results || [];
+    const totalPages = data.total_pages || 1;
 
     return (
         <div>
@@ -36,12 +104,20 @@ const MoviesPage = async () => {
                 <div className={'movies-container'}>
                     {movies.map((movie) => (
                         <div className={'movie-box'} key={movie.id}>
+                            <img
+                                className={'movie-poster'}
+                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                alt={movie.title}
+                            />
                             <h2>{movie.title}</h2>
                             <p>{movie.overview}</p>
                         </div>
                     ))}
                 </div>
             )}
+
+            {/* Пагинация */}
+            <Pagination currentPage={page} totalPages={totalPages} />
         </div>
     );
 };
